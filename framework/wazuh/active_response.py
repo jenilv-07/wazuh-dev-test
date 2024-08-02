b2b3369d0160d8c603991d99b954e349fb88001c
jenilv-07 <vekariyajenil888@gmail.com>
2024-08-02 12:35:16 +0530

    update the normal loop to async loop

--- framework/wazuh/active_response.py
+++ framework/wazuh/active_response.py
@@ -2,6 +2,7 @@
 # Created by Wazuh, Inc. <info@wazuh.com>.
 # This program is free software; you can redistribute it and/or modify it under the terms of GPLv2
 
+import asyncio
 from wazuh.core import active_response, common
 from wazuh.core.agent import get_agents_info
 from wazuh.core.exception import WazuhException, WazuhError, WazuhResourceNotFound
@@ -12,9 +13,9 @@ from wazuh.rbac.decorators import expose_resources
 
 @expose_resources(actions=['active-response:command'], resources=['agent:id:{agent_list}'],
                   post_proc_kwargs={'exclude_codes': [1701, 1703]})
-def run_command(agent_list: list = None, command: str = '', arguments: list = None, custom: bool = False,
-                alert: dict = None) -> AffectedItemsWazuhResult:
-    """Run AR command in a specific agent.
+async def run_command(agent_list: list = None, command: str = '', arguments: list = None, custom: bool = False,
+                      alert: dict = None) -> AffectedItemsWazuhResult:
+    """Run AR command in a specific agent asynchronously.
 
     Parameters
     ----------
@@ -39,20 +40,23 @@ def run_command(agent_list: list = None, command: str = '', arguments: list = No
                                       some_msg='AR command was not sent to some agents',
                                       none_msg='AR command was not sent to any agent'
                                       )
-    if agent_list:
-        with WazuhQueue(common.AR_SOCKET) as wq:
-            system_agents = get_agents_info()
-            for agent_id in agent_list:
-                try:
-                    if agent_id not in system_agents:
-                        raise WazuhResourceNotFound(1701)
-                    if agent_id == "000":
-                        raise WazuhError(1703)
-                    active_response.send_ar_message(agent_id, wq, command, arguments, custom, alert)
-                    result.affected_items.append(agent_id)
-                    result.total_affected_items += 1
-                except WazuhException as e:
-                    result.add_failed_item(id_=agent_id, error=e)
-            result.affected_items.sort(key=int)
+    
+    async def process_agent(agent_id):
+        try:
+            if agent_id not in system_agents:
+                raise WazuhResourceNotFound(1701)
+            if agent_id == "000":
+                raise WazuhError(1703)
+            await active_response.send_ar_message(agent_id, wq, command, arguments, custom, alert)
+            result.affected_items.append(agent_id)
+            result.total_affected_items += 1
+        except WazuhException as e:
+            result.add_failed_item(id_=agent_id, error=e)
 
+    if agent_list:
+        system_agents = await get_agents_info()
+        async with WazuhQueue(common.AR_SOCKET) as wq:
+            await asyncio.gather(*(process_agent(agent_id) for agent_id in agent_list))
+
+    result.affected_items.sort(key=int)
     return result
